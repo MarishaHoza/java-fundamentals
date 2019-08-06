8d214760f3ffb04bef67807c901cf1c2f6a0a39d
MarishaHoza <marisha.hoza@gmail.com>
2019-08-05 18:25:08 -0700

    set up file

---
+++ basics/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    // for labs, call them in the main method
+  }
+
+  // write the separate methods down here
+}
